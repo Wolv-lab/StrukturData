71c2336e55785b940749be880e553dd36c343688
Fais_Wildan <faiswildan336@gmail.com>
2026-04-25 14:11:13 +0700

    Add files via upload

---
+++ UTS_Struktur_data_part2/fibonacci.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class fibonacci {
+    public static int fibo(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibo(n - 1) + fibo(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah suku Fibonacci: ");
+        int n = input.nextInt();
+
+        System.out.println("Deret Fibonacci hingga suku ke-" + n + " adalah:");
+
+        for (int i = 0; i <= n; i++) {
+            System.out.print(fibo(i));
+            if (i < n) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
+
